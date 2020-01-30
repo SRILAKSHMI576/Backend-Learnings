814fa011a5034b3345e79fdb5e5b05114e11b2a5
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-01-30 17:45:30 +0530

    Test validator with isURL method

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,3 +10,7 @@ console.log(para)
 //it return true if string is only email other wise its false
 console.log(validator.isEmail("srilakshmigulla576@gmail.com"))
 console.log(validator.isEmail("hello.com"))
+
+//it return true if string is only url other wise its false
+console.log(validator.isURL("hello"))
+console.log(validator.isURL("hello.com"))
