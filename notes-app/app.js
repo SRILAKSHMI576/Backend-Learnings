ef8c995bda1fd2c626799337fd0271e7f01e7bac
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-01-30 21:58:06 +0530

    Practice on Process.argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,21 +1,11 @@
 const validator = require("validator")
 const chalk = require("chalk")
 
-var notes = require("./notes.js")
+const command = process.argv[2]
 
-var para = notes()
+if (command === "add") {
+	console.log("notes added!")
+} else if (command === "remove") {
+	console.log("notes removed!")
+}
 
-console.log(para)
-
-
-//it return true if string is only email other wise its false
-console.log(validator.isEmail("srilakshmigulla576@gmail.com"))
-console.log(validator.isEmail("hello.com"))
-
-//it return true if string is only url other wise its false
-console.log(validator.isURL("hello"))
-console.log(validator.isURL("hello.com"))
-
-//use chalk
-console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
-console.log(chalk.black("sri"))
