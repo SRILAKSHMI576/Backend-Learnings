b375e5d905c6dd5f98d92ef46f7144776526ccdb
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-10 18:27:48 +0530

    Learn about fetch request

---
+++ weather-app/fetchApp.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+
+function getData() {
+	const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic3JpbGFrc2htaWd1bGxhIiwiYSI6ImNrNmdiOHQ5NTJodjIzZXFmdWlicGJndTQifQ.TJyl2j3e-zCSuTCMUvv86A"	
+	fetch(url)
+	.then(res => res.json())
+	.then(json => console.log(json.features[0].text));
+
+}
+getData()
