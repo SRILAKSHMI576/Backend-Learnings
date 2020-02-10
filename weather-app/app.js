071383879205716a62aec1f22aa5d6f465f8f4dc
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-10 15:38:58 +0530

    Add string

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require("request")
 
-const url = "https://api.darksky.net/forecast/a03621f5993565f5f8037090936ab5b2/37.8267,-122.4233"
+const url = "https://api.darksky.net/forecast/a03621f5993565f5f8037090936ab5b2/37.8267,-122.4233?units=si"
 
-request({url: url}, (error, response) => {
-	const data = JSON.parse(response.body)
-	console.log(data.currently)
+request({url: url, json: true}, (error, response) => {
+	console.log(response.body.currently)
+	console.log("It is currently " + response.body.currently.temperature + " degress out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
 })
