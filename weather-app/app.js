1ec6897d8eace989cf5e2dbc481be2d69b53384c
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-10 15:47:40 +0530

    Customize HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ const url = "https://api.darksky.net/forecast/a03621f5993565f5f8037090936ab5b2/3
 
 request({url: url, json: true}, (error, response) => {
 	console.log(response.body.currently)
-	console.log("It is currently " + response.body.currently.temperature + " degress out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
+	console.log(response.body.daily.data[0].summary +" It is currently " + response.body.currently.temperature + " degress out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
 })
