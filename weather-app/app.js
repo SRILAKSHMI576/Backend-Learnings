51b7d9c7a05258a7e5108761d05dc997f48ae9fe
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-11 11:10:48 +0530

    Add error

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,13 +4,18 @@ const url = "https://api.darksky.net/forecast/a03621f5993565f5f8037090936ab5b2/3
 
 request({url: url, json: true}, (error, response) => {
 	//console.log(response.body.currently)
-	//console.log(response.body.daily.data[0].summary +" It is currently " + response.body.currently.temperature + " degress out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
+	if(error){
+		console.log("Unable to connect to weather app")
+	}else{
+	console.log(response.body.daily.data[0].summary +" It is currently " + response.body.currently.temperature + " degress out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
+	}
 })
 
 const url1 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic3JpbGFrc2htaWd1bGxhIiwiYSI6ImNrNmdiOHQ5NTJodjIzZXFmdWlicGJndTQifQ.TJyl2j3e-zCSuTCMUvv86A"
 
 request({url: url1, json: true}, (error, response) => {
-	const latitude = response.body.features[0].center[0]
-	const longitude = response.body.features[0].center[1]
-	console.log(latitude, longitude)
+	 const latitude = response.body.features[0].center[0]
+	 const longitude = response.body.features[0].center[1]
+	//console.log(latitude, longitude)
+	//console.log(error)
 })
