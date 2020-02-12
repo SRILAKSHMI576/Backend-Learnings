703e0b78921349aedea664da16640cb7ff1a47e7
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-12 09:52:54 +0530

    Practice arrow functions

--- notes-app/2-Arrow-Functions.js
+++ notes-app/2-Arrow-Functions.js
@@ -1,1 +1,13 @@
-const 
+const event = {
+	name: "Birthday Party",
+	guestList: ["Andreq", "Jen", "Mike"],
+	printGuestList(){
+		console.log("Guest list for "+ this.name)
+
+		this.guestList.forEach((guest) => {
+			console.log(guest + " is attending "+ this.name)
+		})
+	}
+}
+
+event.printGuestList()
