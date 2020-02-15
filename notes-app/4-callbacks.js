3293c27cd59b4257099921a235265d61de8a15d5
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-15 11:43:58 +0530

    Practice call backs

--- notes-app/4-callbacks.js
+++ notes-app/4-callbacks.js
@@ -1,37 +1,31 @@
-///1..............
-// setTimeout(() => {
-// 	console.log("Two seconds are up")
-// }, 2000)
+setTimeout(() => {
+	console.log("hiiii")
+}, 2000)
 
-// const names = ["Andrew", "Jen", "Jess"]
-// const shortNames = names.filter((name) => {
-// 	return name.length <= 4 
-// })
-// console.log(shortNames)
+const names = ["sri", "Srinivas", "srilakshmi"]
+const result = names.filter(name => {
+	return name.length >= 4
+})
+//console.log(result)
 
-// const geocode = (address, callback) => {
-// 	setTimeout(() => {
-// 		const data = {
-// 			latitude: 0,
-// 			longitude: 0
-// 		}
-// 		callback(data)
-// 	}, 2000)
-// }
-
-// geocode("Philadelphia", (data) =>{
-// 	console.log(data)
-// })
-
-
-///2........
-
-const add = (a,b, callback) => {
+const geoCode = (address, callback) => {
 	setTimeout(() => {
-		callback(a + b)
+		const data = {
+			longitude: 0,
+			latitude: 0
+		}
+		callback(data)
 	}, 2000)
 }
+geoCode("phosvdgn", (data) => {
+	//console.log(data)
+})
 
-add(4, 1, (sum) => {
+const addition = (a, b, callback) => {
+	setTimeout(() => {
+		callback(a + b)
+	},1000)	
+}
+addition(4, 1, (sum) => {
 	console.log(sum)
 })
