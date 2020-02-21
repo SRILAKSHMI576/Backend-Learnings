6d6dfb7a97588ad6bf3752c2ac3b59dafbeda603
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-02-21 10:47:28 +0530

    Search by Index

--- challenges/spaceX/getAll.js
+++ challenges/spaceX/getAll.js
@@ -2,13 +2,28 @@ const axios  = require("axios")
 
 const url = "https://api.spacexdata.com/v3/capsules"
 
-function getAll(){
+//To get all Data
+// function getAll(){
+// 		axios.get(url)
+// 	.then(res => {
+// 		console.log("res",res)
+// 	})
+// 	.catch(err => {
+// 		console.log("Err",err)
+// 	})
+// }
+// getAll()
+
+//Search By Index
+function findByIndex(index){
 		axios.get(url)
 	.then(res => {
-		console.log("res",res)
+		console.log("res",res.data[index])
 	})
 	.catch(err => {
 		console.log("Err",err)
 	})
-}
-getAll()
+	}
+findByIndex(1)
+findByIndex(3)
+findByIndex(10)
