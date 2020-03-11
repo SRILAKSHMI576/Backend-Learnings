aadb521f918743244a3d0c3a31a08f2ed07f6b43
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-11 14:53:20 +0530

    Practice query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,7 +10,15 @@ const app = express()
 
 const publicDirectoryPath = path.join(__dirname, "../public")
 
+app.set("view engine", "hbs")
 app.use(express.static(publicDirectoryPath))
+
+// app.get("", (req, res) => {
+// 	res.render('index', {
+// 		title: "Weather App",
+// 		name: "Andrew Mead"
+// 	})
+// })
 
 app.get("", (req, res) => {
 	res.send("<h1>Hello world</h1>")
@@ -29,10 +37,29 @@ app.get("", (req, res) => {
 // 	res.send("<title>Hello world </title>")
 // })
 
+
+app.get("/products", (req, res) => {
+	if(!req.query.search){
+		return 	res.send({
+			error: "error is there" 
+		})
+	}
+	res.send({
+		products: []
+	})
+})
+
 app.get("/weather", (req, res) => {
+	if(!req.query.address){
+		res.send({
+			error: "there is error string"
+		})
+	}
+	
 	res.send({
 		firstName: "srilakshmi",
-		lastName: "Gulla"
+		lastName: "Gulla",
+		address: req.query.address
 	})
 })
 
