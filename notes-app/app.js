b281d96c1bc6b352d32492bb4681f2dfe5083811
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-23 20:16:12 +0530

    Import files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,13 @@
 const fs = require('fs')
+const add = require("./utils")
+const notes = require("./notes")
 
 fs.writeFileSync("notes.txt", "Welcome to nodejs course.")
 
 fs.appendFileSync("notes.txt", " i am srilakshmi")
+
+const sum = add(4, -2)
+// console.log(sum)
+
+const getNote = notes("Your notes....")
+console.log(getNote)
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log("srilakshmi")
+
+const name = "I am "
+
+const add = function(a, b){
+	return a + b
+}
+
+module.exports = add
