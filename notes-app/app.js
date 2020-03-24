b80b3355ac4cfb8c175ef4b6c412c285c2191e7a
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-24 09:44:45 +0530

    Add color to text using chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const add = require("./utils")
 const notes = require("./notes")
 const validator = require('validator')
+const chalk = require('chalk')
 
 fs.writeFileSync("notes.txt", "Welcome to nodejs course.")
 
@@ -13,8 +14,15 @@ const sum = add(4, -2)
 const getNote = notes("Your notes....")
 console.log(getNote)
 
-//check string is correct or not
+//check string is correct or not using validator
 const string = validator.isEmail("srilakshmigulla576@gmail.com")
 const url = validator.isURL("www.google.com")
 console.log(string)
 console.log(url)
+
+//print success in green color using chalk
+const color = chalk.green("success")
+const color2 = chalk.bold.green("success") // chalk.green.bold("success")
+const color3 = chalk.bold.inverse.green("success")
+const color1 = chalk.blue("hello") + "world" + chalk.green("!")
+console.log(color, color1, color2, color3)
