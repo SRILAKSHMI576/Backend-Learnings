891ea4bc1be13eaca745b6d784e63c7eb69921dc
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-24 09:26:30 +0530

    Import NPM modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const add = require("./utils")
 const notes = require("./notes")
+const validator = require('validator')
 
 fs.writeFileSync("notes.txt", "Welcome to nodejs course.")
 
@@ -11,3 +12,9 @@ const sum = add(4, -2)
 
 const getNote = notes("Your notes....")
 console.log(getNote)
+
+//check string is correct or not
+const string = validator.isEmail("srilakshmigulla576@gmail.com")
+const url = validator.isURL("www.google.com")
+console.log(string)
+console.log(url)
