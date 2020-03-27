0efcc9915d20bb799c5a9ac2c77bb9e750004fff
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-27 10:16:01 +0530

    Work on Promises and callbacks

---
+++ playground/2-callbacks.js
@@ -0,0 +1,14 @@
+const doWorkCallback = (callback) => {
+	setTimeout(() => {
+		// callback('This is my error!', undefined)
+		callback(undefined, 'sri')
+	}, 2000) 
+} 
+
+doWorkCallback((error, result) => {
+	if(error){
+		return console.log(error)
+	}
+
+	console.log(result)
+})
