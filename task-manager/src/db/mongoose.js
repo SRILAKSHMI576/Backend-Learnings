61473e103a66eea0fb2d214b05a54177901d5783
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-31 22:56:14 +0530

    Create user and task data

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const validator = require("validator")
+// const validator = require("validator")
 
 //Connect node and db using mongoose
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
@@ -9,44 +9,44 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
 })
 
 ///create model
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if(!validator.isEmail(value)){
-				throw new Error("Email is invalid")
-			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 7,
-		trim: true,
-		validate(value){
-			if(value.toLowerCase().includes('password')){
-				throw new Error("Password invalid")
-			}
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if(value < 0) {
-				throw new Error("Age must be a positive number")
-			}
-		}
-	}
-})
+// const User = mongoose.model('User', {
+// 	name: {
+// 		type: String,
+// 		required: true,
+// 		trim: true
+// 	},
+// 	email: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 		lowercase: true,
+// 		validate(value) {
+// 			if(!validator.isEmail(value)){
+// 				throw new Error("Email is invalid")
+// 			}
+// 		}
+// 	},
+// 	password: {
+// 		type: String,
+// 		required: true,
+// 		minlength: 7,
+// 		trim: true,
+// 		validate(value){
+// 			if(value.toLowerCase().includes('password')){
+// 				throw new Error("Password invalid")
+// 			}
+// 		}
+// 	},
+// 	age: {
+// 		type: Number,
+// 		default: 0,
+// 		validate(value) {
+// 			if(value < 0) {
+// 				throw new Error("Age must be a positive number")
+// 			}
+// 		}
+// 	}
+// })
 
 //save data to db
 // const me = new User({
@@ -62,25 +62,25 @@ const User = mongoose.model('User', {
 // })
 
 //create another mondel
-const Task = mongoose.model('Task', {
-	description: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	completed: {
-		type: Boolean,
-		default: false
-	}
-})
+// const Task = mongoose.model('Task', {
+// 	description: {
+// 		type: String,
+// 		required: true,
+// 		trim: true
+// 	},
+// 	completed: {
+// 		type: Boolean,
+// 		default: false
+// 	}
+// })
 
-const task = new Task({
-	description: "             HELOOOOO WORLDDDDDD           ",
-	completed: false
-})
+// const task = new Task({
+// 	description: "             HELOOOOO WORLDDDDDD           ",
+// 	completed: false
+// })
 
-task.save().then(() => {
-	console.log(task)
-}).catch((error) => {
-	console.log(error)
-})
+// task.save().then(() => {
+// 	console.log(task)
+// }).catch((error) => {
+// 	console.log(error)
+// })
