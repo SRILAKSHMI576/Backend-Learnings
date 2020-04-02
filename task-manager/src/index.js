335b50c217076e59af457edb89dcdf90d59e22b7
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-04-02 12:03:59 +0530

    Update data using async/await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -79,6 +79,35 @@ app.post("/users", async (req, res) => {
 		}
 })
 
+//Update user
+app.patch("/users/:id", async(req, res) => {
+	const _id = req.params.id
+	const body = req.body
+
+	const updates = Object.keys(req.body)
+	console.log("updates", updates)
+	const allowedUpdates = ['name', 'email', 'password', 'age']
+	console.log("allwoowdd", allowedUpdates)
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+	if(!isValidOperation) {
+		return res.status(400).send({error: "Invalid updates!"})
+	}
+
+	try {
+		const user = await User.findByIdAndUpdate(_id, body, { new: true, runValidators: true })
+		
+		if(!user){
+			return res.status(404).send()
+		}
+
+		res.send(user)
+
+	} catch (e) {
+		res.status(400)
+	}
+})
+
 //Read tasks
 // app.get("/tasks", (req, res) => {
 // 	Task.find({}).then((tasks) => {
@@ -144,6 +173,33 @@ app.post("/tasks", async (req, res) => {
 		}
 })
 
+//Update task
+app.patch("/tasks/:id", async(req, res) => {
+	const _id = req.params.id
+	const task = req.body
+
+	const updates = Object.keys(req.body)
+	const allowedUpdates = ["description","completed"]
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+	if(!isValidOperation){
+		return res.status(400).send({error: "Invalid updates"})
+	}
+	try {
+		const updateTask = await Task.findByIdAndUpdate(_id, task, {new : true, runValidators: true})
+
+		if(!updateTask){
+			return res.status(404).send()
+		}
+
+		res.send(updateTask)
+
+	} catch (e) {
+		res.status(400).send(e)
+	}
+})
+
 app.listen(port, () => {
 	console.log("Server started")
 })
+
